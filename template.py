9d61f31fc7b0f7145249b51f4e18f4ca3c2fb149
pyaracetamol_143mg <alokv0521@gmail.com>
2025-04-02 03:16:45 +0530

    initial project setup

---
+++ template.py
@@ -0,0 +1,54 @@
+# this is for making direcory automatically instead of manually making the file
+# 
+# just do this before implementing the  end to end project
+#  
+import os
+from pathlib import Path
+import logging
+
+#logging string
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
+project_name='kidney_disease_classifier' # you can change this according to the template
+
+
+# now we are saying the type of files that need to be made
+
+list_of_files=[
+      ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "dvc.yaml",
+    "params.yaml",
+    "requirements.txt",
+    "setup.py",
+    "research/trials.ipynb",
+    "templates/index.html"
+]
+
+#looping through the list and then we 
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+
+
+    if filedir !="":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory; {filedir} for the file: {filename}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass
+            logging.info(f"Creating empty file: {filepath}")
+
+
+    else:
+        logging.info(f"{filename} is already exists")
